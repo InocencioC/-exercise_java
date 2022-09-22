731d138035464ccb3c1798893eb1a974d25a5bab
InocencioC <cordeiroturorias@gmail.com>
2022-09-22 11:18:19 +0100

    Operators java

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int k = 10;
+
+        int i = ++k;
+        int j = k--;
+        int x = k;
+
+        System.out.println("i:" + i);
+        System.out.println("j:" + j);
+        System.out.println("x:" + x);
+
+
+
+    }
+}
